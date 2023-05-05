f2c3881147b81acb3253ab917c53cd57eb3afe2d
Joao Andrade Jr <fullservicemanaus@gmail.com>
2023-05-05 10:00:28 -0300

    feat: req 01

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -5,14 +5,18 @@ import Pokemon from './Pokemon';
 import { pokemonType } from '../types';
 
 class Pokedex extends React.Component {
+  state = {
+    index: 0,
+  };
+
   render() {
     const { pokemonList } = this.props;
+    const { index } = this.state;
     return (
       <>
         <h1> Pokédex </h1>
         <div className="pokedex">
-          { pokemonList
-            .map((pokemon) => <Pokemon key={ pokemon.id } pokemon={ pokemon } />) }
+          <Pokemon key={ pokemonList[index].id } pokemon={ pokemonList[index] } />
         </div>
       </>
     );
